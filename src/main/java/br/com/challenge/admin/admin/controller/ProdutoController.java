ada8acabd06ca9e17b57a679c2e8fd807b261374
Victor Leite <victor_leite@outlook.com.br>
2024-12-06 01:49:37 -0300

    add new methods in produto controller and repository

--- src/main/java/br/com/challenge/admin/admin/controller/ProdutoController.java
+++ src/main/java/br/com/challenge/admin/admin/controller/ProdutoController.java
@@ -3,10 +3,11 @@ package br.com.challenge.admin.admin.controller;
 import br.com.challenge.admin.admin.domain.Produto;
 import br.com.challenge.admin.admin.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin/produtos")
@@ -19,5 +20,30 @@ public class ProdutoController {
     public String addProduto(@RequestBody Produto produto) {
         produtoRepository.save(produto);
         return "Produto adicionado com sucesso!";
+    }
+
+    /*
+     * Retorna todos os produtos
+     */
+    @GetMapping
+    public List<Produto> getProdutos() {
+        return produtoRepository.findAll();
+    }
+
+    /*
+     * Retorna um produto pelo id
+     */
+    @GetMapping("/{id}")
+    public Produto getProduto(@PathVariable int id) {
+        return produtoRepository.findById(id);
+    }
+
+    /*
+     * Remove um produto pelo id
+     */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable("id") String id) {
+        produtoRepository.delete(Integer.parseInt(id));
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- src/main/java/br/com/challenge/admin/admin/repository/ProdutoRepository.java
+++ src/main/java/br/com/challenge/admin/admin/repository/ProdutoRepository.java
@@ -1,10 +1,12 @@
 package br.com.challenge.admin.admin.repository;
 
+import br.com.challenge.admin.admin.domain.Produto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
-import br.com.challenge.admin.admin.domain.Produto;
+import java.util.List;
 
 @Repository
 public class ProdutoRepository {
@@ -20,5 +22,17 @@ public class ProdutoRepository {
     public void delete(int id) {
         String sql = "DELETE FROM produtos WHERE id = ?";
         jdbcTemplate.update(sql, id);
+    }
+
+    public List<Produto> findAll() {
+        String sql = "SELECT * FROM produtos";
+        RowMapper<Produto> rowMapper = (rs, rowNum) -> new Produto(rs.getInt("id"), rs.getString("nome"), rs.getDouble("preco"));
+        return jdbcTemplate.query(sql, rowMapper);
+    }
+
+    public Produto findById(int id) {
+        String sql = "SELECT * FROM produtos WHERE id = ?";
+        RowMapper<Produto> rowMapper = (rs, rowNum) -> new Produto(rs.getInt("id"), rs.getString("nome"), rs.getDouble("preco"));
+        return jdbcTemplate.queryForObject(sql, rowMapper, id);
     }
 }
